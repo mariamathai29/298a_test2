0a87a6f984a2b5100fe07bc89ff6712c33ee4e80
Maria Mathai <121696991+mariamathai29@users.noreply.github.com>
2026-09-17 11:25:58 -0400

    implement counter

--- src/project.v
+++ src/project.v
@@ -5,23 +5,31 @@
 
 `default_nettype none
 
-module tt_um_example (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
+module counter (
+
+    input load,
+    input rst,
+    input [7:0] data_in,
+    input clk,
+
+    output oe,
+    output [7:0] count
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+logic [7:0] count_r;
 
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+always_ff @(posedge clk or posedge rst) begin
+    if (rst) begin
+        count_r <= 'd0;
+    end else begin
+        if (load) begin
+            count_r <= data_in;
+        end else begin
+            count_r <= count+1;
+        end
+    end
+end
+
+assign count = oe ? count_r : 8'bz;
 
 endmodule
